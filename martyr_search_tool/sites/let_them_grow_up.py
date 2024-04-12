c30288c61d672f1bc0643ebea2ae0bc70764e416
ece-mohammad <kuro.ece@gmail.com>
2024-04-12 18:46:11 +0200

    - Added `let them grow up` site to the search sites. - Changed CurlGrepSite's regular expresion method from `re.match` to   `re.searchmethod from `re.match` to `re.search`. - Strip white spaces from appended lines that contain matching name.

---
+++ martyr_search_tool/sites/let_them_grow_up.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+# letthemgrowup.com
+
+The site contains a table with the names of martyred children and teens,
+their names, and links to the sources.
+
+## Strategy
+
+1. Get the website's page https://letthemgrowup.com/children-we-already-lost-en/.
+2. Search the page source for the name.
+
+"""
+
+from martyr_search_tool.sites import base_site
+
+
+class LetThemGrowUp(base_site.CurlGrepSite):
+    Name: str = "LetThemGrowUp"
+    HomePage: str = "https://letthemgrowup.com"
+    QueryTemplate: str = (
+        "https://letthemgrowup.com/children-we-already-lost-en/"
+    )
+
+
+if __name__ == "__main__":
+    ...
